5257b056b22da259d2a0955bdb3978f504fc2987
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-21 00:50:58 +0200

    change BetsConsumer abstract class to a mixin

--- lib/consumers/BetsConsumer.dart
+++ lib/consumers/BetsConsumer.dart
@@ -1,22 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:long_term_bets/consumers/BetConsumer.dart';
 import 'package:long_term_bets/data/Bets.dart';
 import 'package:provider/provider.dart';
 
-abstract class BetsConsumer extends StatelessWidget {
-  final int betIndex;
-  final bool isCompletedList;
-
-  BetsConsumer({
-    @required this.betIndex,
-    @required this.isCompletedList,
-  });
-
-  Widget build(BuildContext context) {
-    Bets bets = Provider.of<Bets>(context);
-    List<Bet> betsArray = isCompletedList ? bets.completedBets : bets.allBets;
-
-    return renderChild(context, betsArray[betIndex], bets);
+mixin BetsConsumer on Widget {
+  Widget betProvider(Bet bet, BetConsumer child) {
+    return ChangeNotifierProvider(
+      builder: (_) => bet,
+      child: child
+    );
   }
 
-  Widget renderChild(BuildContext context, Bet bet, Bets bets);
+  Bets consumeBets(BuildContext context) => Provider.of<Bets>(context);
+
+  List<Bet> betsList(BuildContext context, bool isCompletedList) {
+    Bets bets = consumeBets(context);
+    return isCompletedList ? bets.completedBets : bets.allBets;
+  }
+
+
 }
