312806fd7f1cbeeac3b51d85454291666bf43df1
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-20 15:51:10 +0200

    add AppColors class

---
+++ lib/styles/AppColors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+class AppColors {
+static Color primary = Colors.pink[800];
+static Color secondary = Colors.blueGrey;
+static Color success = Colors.teal;
+static Color danger = Colors.redAccent;
+static Color cardTitle = Colors.grey[700];
+static Color cardText = Colors.grey[500];
+static Color buttonText = Colors.grey[300];
+}
