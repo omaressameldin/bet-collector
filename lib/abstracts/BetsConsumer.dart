d538830f11ac5efaf690e604b2c4971956a0e91d
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-20 15:50:29 +0200

    Add Bets Consumer class

---
+++ lib/abstracts/BetsConsumer.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:long_term_bets/data/Bets.dart';
+import 'package:provider/provider.dart';
+
+abstract class BetsConsumer extends StatelessWidget {
+  final int betIndex;
+  final bool isCompletedList;
+
+  BetsConsumer({
+    @required this.betIndex,
+    @required this.isCompletedList,
+  });
+
+  Widget build(BuildContext context) {
+    // Bets bets = Provider.of<Bets>(context);
+    // List<Bet> betsArray = isCompletedList ? bets.completedBets : bets.allBets;
+
+    // return renderChild(context, betsArray[betIndex], bets);
+
+    return Consumer<Bets>(
+      builder: (context, bets, child) {
+        List<Bet> betsArray = isCompletedList ? bets.completedBets : bets.allBets;
+
+        return renderChild(context, betsArray[betIndex], bets);
+      },
+    );
+  }
+
+  Widget renderChild(BuildContext context, Bet bet, Bets bets);
+}
