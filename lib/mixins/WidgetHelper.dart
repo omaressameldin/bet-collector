6f79c19f8a011f9c3aff43039c00ae6be5014540
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-20 15:50:46 +0200

    Add WidgetHelper mixin

---
+++ lib/mixins/WidgetHelper.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:long_term_bets/styles/AppColors.dart';
+mixin WidgetHelper {
+  Container buildDividedContainer(bool isBig, Widget child, {bool isLast: false}) {
+    double rightPadding = isBig ? 12.0 : 6.0;
+    double leftPadding = isBig ? 0 : 6.0;
+
+    return Container(
+          padding: EdgeInsets.only(right: rightPadding, left: leftPadding),
+          decoration: new BoxDecoration(
+            border: isLast ? null : new Border(right: new BorderSide(
+              width: 1.0 ,
+              color: AppColors.secondary
+            ))
+          ),
+          child: child,
+      );
+  }
+}
