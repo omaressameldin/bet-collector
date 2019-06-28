3448acf04679b7fc8959e6339c21471e40152edc
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-28 12:19:35 +0200

    Add tabs for running, won and lost bets

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,8 @@ class NavigationState extends State<Navigation> with BetsProvider {
   final List<Widget> _children = <Widget>[
     BetsList(betsType: BetsType.all),
     BetsList(betsType: BetsType.running),
+    BetsList(betsType: BetsType.won),
+    BetsList(betsType: BetsType.lost),
   ];
 
   @override
@@ -68,32 +70,50 @@ class NavigationState extends State<Navigation> with BetsProvider {
         backgroundColor: AppColors.primary,
       ),
       body: provideBets(bets, better, _children[_currentIndex]),
+      floatingActionButton: FloatingActionButton.extended(
+        elevation: 4.0,
+        icon: const Icon(Icons.add),
+        label: const Text('Add new'),
+        onPressed: () {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       bottomNavigationBar: BottomNavigationBar(
         elevation: 8.0,
         currentIndex: _currentIndex,
-        selectedItemColor: AppColors.primary,
         unselectedItemColor: AppColors.secondary,
+        showUnselectedLabels: true,
         onTap: onTabTapped,
         items: <BottomNavigationBarItem>[
           BottomNavigationBarItem(
             icon: Icon(AppIcons.allBets),
-            title: const Text('All'),
+            activeIcon: Icon(AppIcons.allBets, color: AppColors.primary),
+            title: Container(height: 0.0),
           ),
           BottomNavigationBarItem(
             icon: Icon(AppIcons.runningBets),
-            title: const Text('Running'),
+            title: Container(height: 0.0),
+            activeIcon: Icon(AppIcons.runningBets, color: AppColors.info),
           ),
           BottomNavigationBarItem(
             icon: Icon(AppIcons.betWinner),
-            title: const Text('Won'),
+            activeIcon: Icon(AppIcons.betWinner, color: AppColors.winner),
+            title: Container(height: 0.0),
           ),
           BottomNavigationBarItem(
             icon: Icon(AppIcons.betLoser),
-            title: const Text('Lost'),
+            activeIcon: Icon(AppIcons.betLoser, color: AppColors.danger),
+            title: Container(height: 0.0),
           ),
           BottomNavigationBarItem(
-            icon: Avatar(size: avatarSize.medium, avatar: better.avatar),
-            title: const Text('Lost'),
+            icon: Container(
+              foregroundDecoration: BoxDecoration(
+                color: AppColors.buttonText,
+                backgroundBlendMode: BlendMode.saturation,
+              ),
+            child: Avatar(size: AvatarSize.medium, avatar: better.avatar),
+            ),
+            activeIcon: Avatar(size: AvatarSize.medium, avatar: better.avatar),
+            title: Container(height: 0.0),
           )
         ],
       ),
