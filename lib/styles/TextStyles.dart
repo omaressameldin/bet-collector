73e974cdfdfa0d8faa78072b55653fdefebff5b7
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-28 14:46:24 +0200

    Add ligtTtitle TextStyle

--- lib/styles/TextStyles.dart
+++ lib/styles/TextStyles.dart
@@ -7,6 +7,11 @@ class TextStyles {
     color: AppColors.cardTitle,
     fontWeight: FontWeight.bold
   );
+  static TextStyle lightTitleStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    color: AppColors.buttonText,
+    fontSize: 30.0
+  );
   static TextStyle tooltipStyle = TextStyle(
     color: AppColors.cardText,
     fontWeight: FontWeight.bold
