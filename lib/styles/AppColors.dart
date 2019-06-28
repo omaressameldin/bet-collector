b2a87df25ac2469cce4988105bd84ab24f10f07c
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-28 12:19:54 +0200

    Edit AppColors

--- lib/styles/AppColors.dart
+++ lib/styles/AppColors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 class AppColors {
-static Color primary = Colors.pink[800];
+static Color primary = Colors.deepPurple[700];
 static Color secondary = Colors.blueGrey;
 static Color info = Colors.blue[800];
 static Color success = Colors.teal;
@@ -8,6 +8,6 @@ static Color danger = Colors.redAccent;
 static Color cardTitle = Colors.grey[700];
 static Color cardText = Colors.grey[500];
 static Color buttonText = Colors.grey[300];
-static Color winner = Colors.amber;
+static Color winner = Colors.amber[700];
 static const Color transparent = Color(0x00000000);
 }
