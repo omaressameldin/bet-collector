a74c50a626c8fa4b56e0788edd251c0f0f9e5270
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-06-29 12:37:24 +0200

    Add first draft of BetDetails

---
+++ lib/widgets/BetCard/BetDetails.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+import 'package:long_term_bets/data/Bets.dart';
+import 'package:long_term_bets/mixins/WidgetHelper.dart';
+import 'package:long_term_bets/styles/AppColors.dart';
+import 'package:long_term_bets/styles/AppIcons.dart';
+import 'package:long_term_bets/styles/AppSizes.dart';
+import 'package:long_term_bets/styles/TextStyles.dart';
+import 'package:long_term_bets/widgets/Translucent/Translucent.dart';
+
+class BetDetails extends StatelessWidget with WidgetHelper {
+  const BetDetails({ @required this.bet });
+
+  final Bet bet;
+
+  @override
+  Widget build(BuildContext context) {
+    return
+        Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            horizontalDivider(),
+            _field(bet.description, AppIcons.bet, 'Descrpiption'),
+            _field(bet.payment, AppIcons.payment, 'Payment'),
+            _moneyPanda(),
+          ],
+    );
+  }
+
+  Widget _field(String field, IconData icon, String fieldName) {
+    return Padding(
+      padding: EdgeInsets.only(
+        top: AppSizes.widgetMargin,
+        bottom: AppSizes.widgetMargin
+      ),
+      child: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Wrap(
+              spacing: AppSizes.iconSpacing,
+              crossAxisAlignment: WrapCrossAlignment.center,
+              children: <Widget>[
+                Icon(icon, color: AppColors.primary,),
+                Text(fieldName, style: TextStyle(color: AppColors.funky))
+              ]
+            ),
+            Text(
+              field,
+              style: TextStyles.detailsStyle,
+            ),
+          ],
+        )
+      )
+    );
+  }
+
+  Widget _moneyPanda() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Translucent(
+              shape: BoxShape.circle,
+              child: Image(
+                image: const AssetImage('assets/images/money-panda.png'),
+                height: AppSizes.mediumImage,
+              ),
+            ),
+          ],
+        )
+      ]
+    );
+  }
+}
