a1b67b3a2726b2055cf121140d1a282a2c592682
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-09-26 00:18:11 +0200

    Fix analyzer errors

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:long_term_bets/consumers/BetsConsumer.dart';
-import 'package:long_term_bets/consumers/BetterConsumer.dart';
-import 'package:long_term_bets/data/Bets.dart';
-import 'package:long_term_bets/data/Better.dart';
-import 'package:long_term_bets/providers/BetsProvider.dart';
 import 'package:long_term_bets/widgets/SignIn/SignInPage.dart';
 
 void main() => runApp(MyApp());
