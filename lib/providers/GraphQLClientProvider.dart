f67a34d26f4df8a8ab03a1079d421610c760b3ec
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-19 10:16:44 +0100

    Add Graphql plugin

---
+++ lib/providers/GraphQLClientProvider.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+mixin GraphQLClientProvider {
+  Widget provideGraphQLClient(Widget child) {
+    final HttpLink httpLink = HttpLink(
+      uri: 'http://192.168.178.34:8500/graphql',
+    );
+
+    // final AuthLink authLink = AuthLink(
+      // getToken: () async => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+      // OR
+      // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
+    // );
+
+    // final Link link = authLink.concat(httpLink);
+
+    final ValueNotifier<GraphQLClient> client = ValueNotifier(
+      GraphQLClient(
+        cache: InMemoryCache(),
+        link: httpLink,
+      ),
+    );
+
+    return GraphQLProvider(
+      client: client,
+      child: child
+    );
+  }
+}
