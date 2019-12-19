59348d8247626817db1721c0a02aa6f1c76fc9aa
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-19 10:16:08 +0100

    Update flutter version

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/home/omaressameldin/flutter"
-export "FLUTTER_APPLICATION_PATH=/home/omaressameldin/work/long_term_bets"
+export "FLUTTER_APPLICATION_PATH=/home/omaressameldin/work/long-term-bets/flutter-app"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
