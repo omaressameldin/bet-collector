a1479125d0d9d0112dc576ce643eb78fd251a3db
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-19 10:14:04 +0100

    Refactor login related functions to be in LoginHelper mixin

--- lib/mixins/LoginHelepr.dart
+++ lib/mixins/LoginHelepr.dart
@@ -1,10 +1,11 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:long_term_bets/data/Better.dart';
 import 'package:long_term_bets/widgets/Login/LoggingIn.dart';
 
 mixin LoginHelper {
-  void whenLoggingIn(BuildContext context, FirebaseUser user) {
+  void whenLoggedIn(BuildContext context, Better user) {
     Navigator.of(context).push<void>(
       MaterialPageRoute<void>(
         builder: (_) {
@@ -17,5 +18,34 @@ mixin LoginHelper {
   void signOutGoogle(BuildContext context) async{
     await GoogleSignIn().signOut();
     Navigator.of(context).pop();
+  }
+
+  Future<Better> getLoggedInUser(BuildContext context) async{
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final FirebaseUser currentUser = await auth.currentUser();
+    final String token = await currentUser.getIdToken();
+
+     if(token != null) {
+      return await Better.login(context, token);
+     }
+
+     return null;
+  }
+
+  Future<Better> signInUser(BuildContext context) async {
+    final FirebaseAuth auth = FirebaseAuth.instance;
+    final GoogleSignInAccount googleSignInAccount = await GoogleSignIn().signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication = await googleSignInAccount.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final FirebaseUser user = await auth.signInWithCredential(credential);
+    final String token = await user.getIdToken();
+    assert(!user.isAnonymous);
+    assert(token != null);
+
+    return await Better.login(context, token);
   }
 }
