3c7863dd82d92a81d4af5454791d6958e7a24ba7
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-19 10:34:34 +0100

    Add login Function to Better

--- lib/data/Better.dart
+++ lib/data/Better.dart
@@ -1,58 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:long_term_bets/mixins/QueriesHelper.dart';
 List<Better> betters = <Better>[
   Better(
+    email: 'test@test.test',
     id: '1',
     name: 'Shadwa',
     avatar: NetworkImage('https://listimg.pinclipart.com/picdir/s/335-3356471_female-avatar-girls-avatar-clipart.png'),
   ),
   Better(
+    email: 'test@test.test',
     id: '2',
     name: 'Omar',
     avatar: NetworkImage('https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png'),
   ),
   Better(
+    email: 'test@test.test',
     id: '3',
     name: 'Aley',
     avatar: NetworkImage('https://banner2.kisspng.com/20180626/fhs/kisspng-avatar-user-computer-icons-software-developer-5b327cc98b5780.5684824215300354015708.jpg')
   ),
   Better(
+    email: 'test@test.test',
     id: '3',
     name: 'Mody',
     avatar: NetworkImage('http://www.sclance.com/pngs/avatar-icon-png/avatar_icon_png_70847.jpg')
   ),
   Better(
+    email: 'test@test.test',
     id: '1',
     name: 'Mehany',
     avatar: NetworkImage('https://img.pngio.com/child-avatar-icon-flat-design-red-yellow-coffee-avatar-png-693_720.png'),
   ),
   Better(
+    email: 'test@test.test',
     id: '2',
     name: 'Hariry',
     avatar: NetworkImage('https://cdn0.iconfinder.com/data/icons/iconshock_guys/512/andrew.png'),
   ),
   Better(
+    email: 'test@test.test',
     id: '3',
     name: 'sharkawy',
     avatar: NetworkImage('http://kepido.com/protected/modules/singlemerchant/assets/images/avatar.png')
   ),
   Better(
+    email: 'test@test.test',
     id: '3',
     name: 'Mody',
     avatar: NetworkImage('http://www.sclance.com/pngs/avatar-icon-png/avatar_icon_png_70847.jpg')
   ),
 ];
 
-class Better {
+class Better with QueriesHelper {
 
   Better({
     @required this.id,
     @required this.name,
     @required this.avatar,
+    @required this.email,
   });
 
   final String id;
   final String name;
   final NetworkImage avatar;
+  final String email;
 
  @override
   bool operator ==(dynamic other) {
@@ -71,6 +83,17 @@ class Better {
         return b.name.toLowerCase().contains(input.toLowerCase());
       }).toList();
     });
+  }
 
+  static Future<Better> login(BuildContext context, String token) async {
+  final QueryResult res = await QueriesHelper.makeQuery(context, QueriesHelper.loginUser(token));
+  final Map<String, dynamic> userResult =res.data['loginUser']['user'];
+
+  return Better(
+    avatar: NetworkImage(userResult['avatar']),
+    email: userResult['email'],
+    name: userResult['name'],
+    id: userResult['id']
+    );
   }
 }
