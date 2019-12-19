26d934646c462e8dfb55f345ad72f8854b0f40e1
Omar Essam (Inpu) <o.essameldin@gmail.com>
2019-12-19 10:33:07 +0100

    Add QueriesHelper mixin

---
+++ lib/mixins/QueriesHelper.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+
+mixin QueriesHelper {
+
+  static Future<QueryResult> makeQuery(BuildContext context, String document) async {
+    final GraphQLClient client = GraphQLProvider.of(context).value;
+    final QueryResult res = await client.mutate(MutationOptions(
+      document: document
+    ));
+
+    return res;
+  }
+
+  static String loginUser(String token) {
+    return """
+      mutation{
+        loginUser(input: {
+          api: "v1",
+          auth_id: "$token",
+        }){
+          user {
+            id
+            name
+            email
+            avatar
+          }
+        }
+      }
+    """;
+  }
+}
